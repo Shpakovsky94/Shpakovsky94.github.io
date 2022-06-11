49b9d112c0fbae58b914b77db8ee884e53930b1e
oleks <shpakovsky94@gmail.com>
2022-06-11 15:39:34 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+// import MainPage from './components/main-page/MainPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // MainPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  box-sizing: border-box;
+  background-color: #f3f3f3;
+}
+a {
+  white-space:pre-wrap;
+}
+</style>
